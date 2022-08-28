78ca4ca9307e21d0421e9cdb592ecb845625f3ee
Andrew <andrewvad517@gmail.com>
2022-08-28 19:10:33 -0400

    add back function

--- src/main/kotlin/com/craftschemers/hub/minigame/Minigame.kt
+++ src/main/kotlin/com/craftschemers/hub/minigame/Minigame.kt
@@ -4,5 +4,8 @@ import com.craftschemers.hub.HubPlayer
 
 abstract class Minigame(val name: String, val type: GameType) {
     var state = GameState.LOBBY
+    fun addPlayer(player: HubPlayer) {
+        // implement later
+    }
     abstract fun start()
 }
