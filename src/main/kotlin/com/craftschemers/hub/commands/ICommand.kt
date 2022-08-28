42168e45d73efe0be9018e902ca299b0104a4812
Andrew <andrewvad517@gmail.com>
2022-08-28 15:02:43 -0400

    mark review

--- src/main/kotlin/com/craftschemers/hub/commands/ICommand.kt
+++ src/main/kotlin/com/craftschemers/hub/commands/ICommand.kt
@@ -4,20 +4,14 @@ import com.craftschemers.hub.minigame.Minigame
 import org.bukkit.command.CommandSender
 
 interface ICommand {
+
     val name: String
-
     val aliases: Array<String>?
-
     val canBeConsole: Boolean
-
     val description: String
-
     val parameters: Array<String>?
-
     val usage: Array<String>
-
     val permissionMessage: String
-
     val permission: String
 
     fun onCommand(sender: CommandSender, minigame: Minigame, label: String, args: Array<String>): Boolean
